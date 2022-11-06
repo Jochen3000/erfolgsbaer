9998befa3b3f0d1d1025bc8997147da9edf933c0
jochen ade <jochen.ade@hotmail.com>
2022-11-06 12:50:03 +0100

    initial setup

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('hallo meine freunde');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hey! Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
