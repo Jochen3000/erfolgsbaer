b02763e44fe624584b6383464e40d5a46bbd9afa
jochen ade <jochen.ade@hotmail.com>
2022-11-26 16:09:56 +0100

    local storage journal works

--- screens/LearnUse.js
+++ screens/LearnUse.js
@@ -1,31 +1,52 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function LearnUse() {
 
+    // declare arrays
+    const [myStorageVals, setMyStorageVals] = useState('init');
     const [myArray, setArray] = useState([
-        { name: 'jo', id: '1' },
-        { name: 'biff', id: '2' }
+        { title: 'yesterday all my trouble', id: '1' },
+        { title: '99 red balloons', id: '2' }
     ]);
 
-    const [newItem, setNewItem] = useState({ name: 'micky', id: '3' })
-
-    const updateVals = () => {
-        setArray((oldarray) => [...oldarray, newItem])
+    // write data
+    const storeData = async () => {
+        try {
+            const jsonValue = JSON.stringify(myArray)
+            await AsyncStorage.setItem('storageJournalEntry', jsonValue)
+        } catch (e) {
+            // saving error
+            console.log('saving problem')
+        }
     }
+    useEffect(() => {
+        storeData();
+    }, []);
 
+    // read storage and put in variable
+    const getStoredData = async () => {
+        try {
+            const value = await AsyncStorage.getItem('storageJournalEntry')
+            if (value !== null) {
+                const data = JSON.parse(value);
+                // put data in array
+                setMyStorageVals(value);
+            }
+        } catch (e) {
+            // error reading value
+            console.log('da sind keine daten')
+        }
+    }
+    useEffect(() => {
+        getStoredData();
+    }, []);
+    console.log('meine Werte', myStorageVals)
     return (
         <View style={styles.container}>
-            {myArray.map((item) => (
-                <View key={item.id}>
-                    <Text>{item.name}</Text>
-                </View>
-            ))}
-            <Button
-                onPress={updateVals}
-                title="Learn More" />
+            <Text>Hallo</Text>
         </View>
-
     );
 }
 
