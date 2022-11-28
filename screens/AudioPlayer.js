7af68c6865b134998c3979f93a092f9d08172a53
jochen ade <jochen.ade@hotmail.com>
2022-11-28 17:32:35 +0100

    progress bar working

--- screens/AudioPlayer.js
+++ screens/AudioPlayer.js
@@ -11,23 +11,31 @@ const AudioPlayer = () => {
     const [duration, setDuration] = useState(null);
     const [position, setPosition] = useState(null);
 
+
     async function playSound() {
         console.log('Loading Sound');
-        const { sound } = await Audio.Sound.createAsync(require('../assets/let-not.mp3')
-        );
+        const { sound } = await Audio.Sound.createAsync(require('../assets/let-not.mp3'));
         setSound(sound);
+
+        sound.setOnPlaybackStatusUpdate((status) => {
+            console.log(status.positionMillis);
+            setPosition(status.positionMillis);
+        });
 
         console.log('Playing Sound');
         await sound.playAsync();
         setIsPlaying(true);
+        const Status = await sound.getStatusAsync();
+        console.log('duration', Status.durationMillis);
+        console.log('position', Status.positionMillis);
+        setDuration(Status.durationMillis);
+        setPosition(Status.positionMillis);
     }
 
     async function pauseSound() {
         console.log('pausing sound');
         await sound.pauseAsync();
-        const Status = await sound.getStatusAsync()
         setIsPlaying(false);
-        console.log('position', Status)
     }
 
     const onPlayPausePress = () => {
@@ -38,7 +46,7 @@ const AudioPlayer = () => {
         }
     }
 
-    React.useEffect(() => {
+    useEffect(() => {
         return sound
             ? () => {
                 console.log('Unloading Sound');
@@ -47,11 +55,21 @@ const AudioPlayer = () => {
             : undefined;
     }, [sound]);
 
+    const getProgress = () => {
+        if (sound === null || duration === null || position === null) {
+            return 0;
+        }
+
+        return (position / duration) * 100;
+    }
+
     return (
         <View style={styles.container}>
-            <View style={[styles.progress, { width: `100%` }]} />
+            <View style={[styles.progress, { width: `${getProgress()}%` }]} />
             <View style={styles.row}>
                 <View style={styles.iconsContainer}>
+                    <Text style={styles.description}>Duration: {duration} </Text>
+                    <Text style={styles.description}>Postion: {position}  </Text>
                     <TouchableOpacity>
                         <FontAwesome name={isPlaying ? 'pause' : 'play'} onPress={onPlayPausePress} size={30} color={"white"} />
                     </TouchableOpacity>
@@ -73,15 +91,19 @@ const styles = StyleSheet.create({
         height: 10,
         backgroundColor: '#bcbcbc'
     },
-    row: {
-        flexDirection: 'row',
+    description: {
+        color: 'white',
+        fontWeight: 'bold'
     },
-    iconsContainer: {
-        flexDirection: 'row',
+    row: {
+        flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        marginTop: 25,
-        width: '100%'
+    },
+    iconsContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
 })
 
