f074ce00376d340efd327289478a1a4b709ccfd3
jochen ade <jochen.ade@hotmail.com>
2022-11-29 13:41:02 +0100

    progress slider working

--- screens/AudioPlayer.js
+++ screens/AudioPlayer.js
@@ -2,23 +2,22 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 import { FontAwesome } from "@expo/vector-icons";
 import { Audio } from 'expo-av';
+import Slider from '@react-native-community/slider';
 
 const AudioPlayer = () => {
 
-    const [song, setSong] = useState(null);
     const [sound, setSound] = useState(null);
     const [isPlaying, setIsPlaying] = useState(false);
     const [duration, setDuration] = useState(null);
     const [position, setPosition] = useState(null);
-
 
     async function playSound() {
         console.log('Loading Sound');
         const { sound } = await Audio.Sound.createAsync(require('../assets/let-not.mp3'));
         setSound(sound);
 
+        // track progress
         sound.setOnPlaybackStatusUpdate((status) => {
-            console.log(status.positionMillis);
             setPosition(status.positionMillis);
         });
 
@@ -26,8 +25,6 @@ const AudioPlayer = () => {
         await sound.playAsync();
         setIsPlaying(true);
         const Status = await sound.getStatusAsync();
-        console.log('duration', Status.durationMillis);
-        console.log('position', Status.positionMillis);
         setDuration(Status.durationMillis);
         setPosition(Status.positionMillis);
     }
@@ -49,7 +46,6 @@ const AudioPlayer = () => {
     useEffect(() => {
         return sound
             ? () => {
-                console.log('Unloading Sound');
                 sound.unloadAsync();
             }
             : undefined;
@@ -59,21 +55,34 @@ const AudioPlayer = () => {
         if (sound === null || duration === null || position === null) {
             return 0;
         }
-
         return (position / duration) * 100;
     }
 
+    const myProgress = getProgress();
+
     return (
-        <View style={styles.container}>
-            <View style={[styles.progress, { width: `${getProgress()}%` }]} />
-            <View style={styles.row}>
-                <View style={styles.iconsContainer}>
-                    <Text style={styles.description}>Duration: {duration} </Text>
-                    <Text style={styles.description}>Postion: {position}  </Text>
-                    <TouchableOpacity>
-                        <FontAwesome name={isPlaying ? 'pause' : 'play'} onPress={onPlayPausePress} size={30} color={"white"} />
-                    </TouchableOpacity>
+        <View>
+            <View style={styles.player}>
+
+                <View style={[styles.progress, { width: `${getProgress()}%` }]} />
+                <View style={styles.row}>
+                    <View style={styles.iconsContainer}>
+                        <Text style={styles.description}>Duration: {duration} </Text>
+                        <TouchableOpacity>
+                            <FontAwesome name={isPlaying ? 'pause' : 'play'} onPress={onPlayPausePress} size={30} color={"white"} />
+                        </TouchableOpacity>
+                    </View>
                 </View>
+            </View>
+            <View style={styles.slidercontainer}>
+                <Slider
+                    style={styles.slider}
+                    minimumValue={0}
+                    maximumValue={100}
+                    minimumTrackTintColor="tomato"
+                    maximumTrackTintColor="lightgrey"
+                    value={myProgress}
+                />
             </View>
         </View>
     )
@@ -81,11 +90,13 @@ const AudioPlayer = () => {
 
 const styles = StyleSheet.create({
     container: {
-        position: 'absolute',
-        bottom: 300,
+        flex: 1,
+    },
+    player: {
         backgroundColor: 'tomato',
         width: '100%',
-        height: 100,
+        height: 120,
+        marginTop: 60,
     },
     progress: {
         height: 10,
@@ -104,6 +115,16 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    slidercontainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    },
+    slider: {
+        width: 300,
+        height: 40,
+        marginTop: 40,
     },
 })
 
