62f5622515cc39493d47b117288cd4afedd2bc66
jochen ade <jochen.ade@hotmail.com>
2022-12-18 15:18:52 +0100

    splash screen working

--- App.js
+++ App.js
@@ -3,16 +3,19 @@ import { StyleSheet } from 'react-native';
 import { useEffect, useState } from 'react';
 import authContext from './auth/authContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SplashScreen from 'expo-splash-screen';
 
 import colors from './config/colors';
 import AppNavigator from './navigation/AppNavigator';
 import AuthNavigator from './navigation/AuthNavigator';
 
-function App() {
+SplashScreen.preventAutoHideAsync();
 
+function App() {
+  const [isReady, setIsReady] = useState(false);
   const [user, setUser] = useState(null);
 
-  // check if we have a stored user
+  // get stored user
   useEffect(() => {
     const restoreUser = async () => {
       const storedString = await AsyncStorage.getItem('auth.currentUser');
@@ -20,11 +23,20 @@ function App() {
         const storedUser = JSON.parse(storedString).email;
         console.log('mein user', storedUser);
         setUser(storedUser);
+        setIsReady(true);
+      } else {
+        setIsReady(true);
       }
     };
     restoreUser();
   }, []);
 
+  // hide splash screen when user is set
+  useEffect(() => {
+    if (isReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [isReady]);
 
   return (
     <authContext.Provider value={{ user, setUser }}>
